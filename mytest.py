a95572dc5e48e68e55f70b685b3a469a19f3db15
Emerson Virti <emerson.virti@gmail.com>
2018-07-11 11:23:59 -0300

    Modification to test branch operation

--- mytest.py
+++ mytest.py
@@ -4,8 +4,13 @@ mydict['a']="first"
 mydict['b']="second"
 mydict['3']="third"
 mydict['4']="fourth"
+print ('Keys without sort:')
 print (mydict.keys())
+print ('Dictionary without sort:')
 print (mydict)
 test={}
 test=mydict
+print ('Keys sort:')
+print (sorted(test.keys()))
+print ('Values sort:')
 print (sorted(test.values()))

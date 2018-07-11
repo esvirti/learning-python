e5ea37f3d800dbd478a3998d86798b2ba2376af0
Emerson Virti <emerson.virti@gmail.com>
2018-07-11 11:12:51 -0300

    test order dictionaries

---
+++ mytest.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+mydict={}
+mydict['a']="first"
+mydict['b']="second"
+mydict['3']="third"
+mydict['4']="fourth"
+print (mydict.keys())
+print (mydict)
+test={}
+test=mydict
+print (sorted(test.values()))
